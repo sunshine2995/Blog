ea4fb475ee039a282374640f1106e5cab5fba755
sunshine2995 <30828101+sunshine2995@users.noreply.github.com>
2017-12-27 16:06:42 +0800

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = 'static/'
+
+# STATICFILES_DIRS = (
+    # os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
