fed83afe852369ce4137876d852b9073d7d2a4e8
sunshine2995 <30828101+sunshine2995@users.noreply.github.com>
2017-12-27 16:43:21 +0800

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -134,4 +134,4 @@ STATIC_ROOT = 'static/'
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+# )
